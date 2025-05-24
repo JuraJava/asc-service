61ef1a09803b2b796a309ca159854b122ec53862
Yaroslav Novikov <grinderos.coding@gmail.com>
2025-05-24 16:54:56 +0000

    Initial commit

---
+++ src/main/java/com/yurdan/ascService/AscServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yurdan.ascService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscServiceApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AscServiceApplication.class, args);
+
+    }
+}
