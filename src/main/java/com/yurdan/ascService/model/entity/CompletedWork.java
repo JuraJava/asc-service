e808708c34800a3c75509e25db1487a9784d0a43
YurDan1973 <jura.danilov.73@gmail.com>
2025-05-27 16:21:21 +0300

    Entity

---
+++ src/main/java/com/yurdan/ascService/model/entity/CompletedWork.java
@@ -0,0 +1,35 @@
+package com.yurdan.ascService.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "completed_work")
+public class CompletedWork {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name_work", nullable = false, unique = true)
+    private String nameWork;
+
+    @Column(name = "cost_of_work", nullable = false)
+    private BigDecimal costOfWork;
+
+}
+
+//DIAGNOSTICS
+//PROGRAM_REPAIR
+//UNBLOCKING
+//REPLACEMENT_UNDER_WARRANTY
+//REPLACEMENT_ON_PAID_BASIS
--- src/main/java/com/yurdan/ascService/model/enums/CompletedWork.java
+++
@@ -1,9 +0,0 @@
-package com.yurdan.ascService.model.enums;
-
-public enum CompletedWork {
-    DIAGNOSTICS,
-    PROGRAM_REPAIR,
-    UNBLOCKING,
-    REPLACEMENT_UNDER_WARRANTY,
-    REPLACEMENT_ON_PAID_BASIS;
-}
