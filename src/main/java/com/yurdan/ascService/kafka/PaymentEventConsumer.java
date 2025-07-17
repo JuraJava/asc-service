46065efda69e4179a7aa2ff0e96a32c1ff58292f
YurDan1973 <jura.danilov.73@gmail.com>
2025-07-17 14:45:16 +0300

    Добавление комментариев

--- src/main/java/com/yurdan/ascService/kafka/PaymentEventConsumer.java
+++ src/main/java/com/yurdan/ascService/kafka/PaymentEventConsumer.java
@@ -4,6 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+
+/**
+ * Класс реализует консьюмера (потребителя) Kafka, который
+ * автоматически запускается вместе с Spring приложением,
+ * подписан на определённый Kafka-топик,
+ * получает сообщения в виде строк (JSON, текст),
+ * логирует каждое сообщение в консоль или файл.
+ */
 @Slf4j
 @Component
 public class PaymentEventConsumer {
